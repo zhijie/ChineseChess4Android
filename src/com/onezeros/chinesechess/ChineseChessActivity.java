4c453c752629281e33c1df34d68a4551ed5287c9
zhijie lee <onezeros.lee@gmail.com>
2012-06-26 23:46:22 +0800

    add domob ad

--- src/com/onezeros/chinesechess/ChineseChessActivity.java
+++ src/com/onezeros/chinesechess/ChineseChessActivity.java
@@ -1,9 +1,12 @@
 package com.onezeros.chinesechess;
 
+import cn.domob.android.ads.DomobAdView;
+
 import com.android.chinesechess.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class ChineseChessActivity extends Activity {
 
@@ -11,6 +14,11 @@ public class ChineseChessActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+     // domob ad
+        LinearLayout layout = (LinearLayout)findViewById(R.id.AdLinearLayout);
+        DomobAdView adView = new DomobAdView(this,"56OJyOeouMzH2P6sIM",DomobAdView.INLINE_SIZE_320X50);
+        layout.addView(adView);
     }
     
 }
