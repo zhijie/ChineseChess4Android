272ca47af307aae1ad248d727ca22ba5a084eaf2
zhijie lee <onezeros.lee@gmail.com>
2012-06-27 00:05:37 +0800

    add umeng statistic

--- src/com/onezeros/chinesechess/ChineseChessActivity.java
+++ src/com/onezeros/chinesechess/ChineseChessActivity.java
@@ -3,6 +3,7 @@ package com.onezeros.chinesechess;
 import cn.domob.android.ads.DomobAdView;
 
 import com.android.chinesechess.R;
+import com.umeng.analytics.MobclickAgent;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -20,5 +21,17 @@ public class ChineseChessActivity extends Activity {
         DomobAdView adView = new DomobAdView(this,"56OJyOeouMzH2P6sIM",DomobAdView.INLINE_SIZE_320X50);
         layout.addView(adView);
     }
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		MobclickAgent.onResume(this);
+	}
+
+	@Override
+	protected void onResume() {		
+		super.onResume();
+		MobclickAgent.onPause(this);
+	}
     
 }
